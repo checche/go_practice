ac985271f65f031ed56dab61276508956cdabb6c
Takahiro Hirata <takakun47@gmail.com>
2019-09-21 17:13:57 +0900

    update: lunch

--- functions/lunch/function.go
+++ functions/lunch/function.go
@@ -4,16 +4,45 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
 func Lunch(w http.ResponseWriter, r *http.Request) {
+	// POSTのみを許可する
+	if r.Method != "POST" {
+		e := "Method Not Allowed."
+		log.Println(e)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(e))
+		return
+	}
+
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Println(err)
+		log.Printf("ReadAllError: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
+		return
 	}
-	log.Printf("%v", string(b))
+
+	parsed, err := url.ParseQuery(string(b))
+	if err != nil {
+		log.Printf("ParseQueryError: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// token認証処理
+	if parsed.Get("token") != os.Getenv("SLACK_TOKEN") {
+		e := "Unauthorized Token."
+		log.Printf(e)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(e))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte(parsed.Get("text")))
 }
